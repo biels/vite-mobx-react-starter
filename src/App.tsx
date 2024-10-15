1e3039912c59ed6ae1a8296356d73372497d3f20
biels <bielsimon2@gmail.com>
2024-10-15 16:32:58 +0200

    Updated and added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import {observer} from "mobx-react";
 import {useAppStore} from "./AppStore.ts";
 
 
-const App = observer(() =>  {
+const App = observer(() => {
     let appStore = useAppStore()
-   return  <>
-       <button onClick={() => window.location.href = "/test"}>Go to test</button>
-       <button
-              onClick={() => appStore.setCounter(appStore.count + 1) }
-       >Increment {appStore.count}
-       </button>
-   </>
+    return <>
+        <button onClick={() => window.location.href = "/test"}>Go to test</button>
+        <button
+          onClick={() => appStore.setCounter(appStore.count + 1)}
+        >Increment {appStore.count}
+        </button>
+
+        <div className="bg-red-500">
+            <p className="text-blue-500"> Tailwind CSS is working! </p>
+        </div>
+    </>
 });
 
 export default App
